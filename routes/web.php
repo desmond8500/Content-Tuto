519b97a78999621019576c9d069233a61483b761
kratos <desmond8500@gmail.com>
2022-01-25 07:49:03 +0000

    tuto update

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use App\Http\Livewire\Tutos\Index;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/{file_id?}/{categorie?}', Index::class)->name('index');
+Route::get('/login', Index::class)->name('login');
